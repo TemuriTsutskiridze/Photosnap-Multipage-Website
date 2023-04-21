ed7b4887a5226ba6b27a249c6396acac3739d7c0
MariamKvirchiladze <kvircho555@gmail.com>
2023-04-22 02:21:49 +0400

    Implement animations for burger menu and dropdown menu

--- shared/js/menu.js
+++ shared/js/menu.js
@@ -1,17 +1,16 @@
 `use strict`;
 
-const open = document.querySelector(`#open`);
 const menu = document.querySelector(`#menu-mob`);
-let opened = false;
 
-open.addEventListener(`click`, () => {
-  if (!opened) {
-    menu.classList.remove(`invisible`);
-    open.src = "../../shared/assets/mobile/close.svg";
-    opened = true;
-  } else {
-    menu.classList.add(`invisible`);
-    open.src = "../../shared/assets/mobile/menu.svg";
-    opened = false;
-  }
+const menuBtn = document.querySelector(".menu-btn");
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  showMenu();
 });
+function showMenu() {
+  if (menuBtn.classList.contains(`active`)) {
+    menu.classList.add("active");
+  } else {
+    menu.classList.remove("active");
+  }
+}
