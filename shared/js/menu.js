baf5d8ec0cba9bb1b92bcab3a3a377e038854dff
MariamKvirchiladze <kvircho555@gmail.com>
2023-04-24 23:36:46 +0400

    fix header bug

--- shared/js/menu.js
+++ shared/js/menu.js
@@ -1,6 +1,7 @@
 `use strict`;
 
 const menu = document.querySelector(`#menu-mob`);
+const btn = document.querySelector(`#btn`);
 
 const menuBtn = document.querySelector(".menu-btn");
 menuBtn.addEventListener("click", function () {
@@ -9,8 +10,10 @@ menuBtn.addEventListener("click", function () {
 });
 function showMenu() {
   if (menuBtn.classList.contains(`active`)) {
+    btn.src = `../../shared/assets/mobile/close.svg`;
     menu.classList.add("active");
   } else {
     menu.classList.remove("active");
+    btn.src = `../../shared/assets/mobile/menu.svg`;
   }
 }
